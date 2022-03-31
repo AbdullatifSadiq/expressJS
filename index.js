63f455edfbb2618d0f29334b094e2285caf50e4a
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-31 17:23:31 -0400

    basic api

--- index.js
+++ index.js
@@ -2,7 +2,36 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+// JSON 'file'
+const members = [
+    {
+        id:1,
+        name: 'Shannon Mensah',
+        email:'shannon@gmail.com',
+        status:'active'
 
+    },
+    {
+        id:2,
+        name: 'Kezia Mensah',
+        email:'kezia@gmail.com',
+        status:'inactive'
+
+    },
+    {
+        id:3,
+        name: 'Tariq Mensah',
+        email:'Tariq @gmail.com',
+        status:'inactive'
+
+    },
+]
+
+// Basic REST API that saves JSON through Postman
+
+app.get('/api/members',(req,res)=>{
+    res.json(members)
+})
 //  Set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
