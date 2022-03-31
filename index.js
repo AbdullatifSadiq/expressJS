6ff3084aa809e822006161d09e6f7fdefe00b48d
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-31 16:57:06 -0400

    basic server with express

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Hello world</h1>')
-})
+//  Set static folder
+app.use(express.static(path.join(__dirname,'public')))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 2000
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)) 
