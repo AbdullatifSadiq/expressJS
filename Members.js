4be229ac8b3871f634b0b1582cf9e19a52068f1d
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-31 17:42:03 -0400

    basic middleware that logs a url and a date

---
+++ Members.js
@@ -0,0 +1,26 @@
+
+const members = [
+    {
+        id:1,
+        name: 'Shannon Mensah',
+        email:'shannon@gmail.com',
+        status:'active'
+
+    },
+    {
+        id:2,
+        name: 'Kezia Mensah',
+        email:'kezia@gmail.com',
+        status:'inactive'
+
+    },
+    {
+        id:3,
+        name: 'Tariq Mensah',
+        email:'Tariq @gmail.com',
+        status:'inactive'
+
+    },
+]
+
+module.exports = members
--- index.js
+++ index.js
@@ -1,37 +1,19 @@
 const express = require('express')
 const path = require('path')
+const members = require('./Members')
+const logger = require('./Middleware/logger')
 
 const app = express()
-// JSON 'file'
-const members = [
-    {
-        id:1,
-        name: 'Shannon Mensah',
-        email:'shannon@gmail.com',
-        status:'active'
 
-    },
-    {
-        id:2,
-        name: 'Kezia Mensah',
-        email:'kezia@gmail.com',
-        status:'inactive'
 
-    },
-    {
-        id:3,
-        name: 'Tariq Mensah',
-        email:'Tariq @gmail.com',
-        status:'inactive'
-
-    },
-]
+// Init middleware
+// app.use(logger)
 
 // Basic REST API that saves JSON through Postman
-
 app.get('/api/members',(req,res)=>{
     res.json(members)
 })
+ 
 //  Set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
