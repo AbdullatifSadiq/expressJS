6ab8945cb17d0b924c3604d85b3b46d8749178d5
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-04-02 07:47:42 -0400

    get request for a signle item

--- Members.js
+++ Members.js
@@ -5,21 +5,18 @@ const members = [
         name: 'Shannon Mensah',
         email:'shannon@gmail.com',
         status:'active'
-
     },
     {
         id:2,
         name: 'Kezia Mensah',
         email:'kezia@gmail.com',
         status:'inactive'
-
     },
     {
         id:3,
         name: 'Tariq Mensah',
         email:'Tariq @gmail.com',
         status:'inactive'
-
     },
 ]
 
--- index.js
+++ index.js
@@ -5,15 +5,25 @@ const logger = require('./Middleware/logger')
 
 const app = express()
 
-
 // Init middleware
 // app.use(logger)
 
-// Basic REST API that saves JSON through Postman
+// Basic REST API that saves JSON through Postman // Get all members
 app.get('/api/members',(req,res)=>{
     res.json(members)
+}) 
+
+// Get Single member using ID
+// app.get('/api/members/:id', (req, res)=>{
+//     res.json(members.filter(member => {
+//         member.id === parseInt(req.params.id)
+//     }))
+// })
+app.get('/api/members/:id',(req,res) =>{
+    res.json(members.filter (member => 
+        member.id === parseInt(req.params.id)  ))
 })
- 
+
 //  Set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
